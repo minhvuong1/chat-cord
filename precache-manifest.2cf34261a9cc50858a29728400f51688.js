25cb305df4a08258197ad5d2bb7dced0ae690690
Minh Vuong <minhvuong@live.com>
2020-06-12 11:49:12 +1000

    Updates

---
+++ precache-manifest.2cf34261a9cc50858a29728400f51688.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2abe170b9639f3f461f3c17f59d240d0",
+    "url": "/minhvuong1.github.io/chat-cord/index.html"
+  },
+  {
+    "revision": "fb39e52543dd0cfea743",
+    "url": "/minhvuong1.github.io/chat-cord/static/css/main.84de2ae0.chunk.css"
+  },
+  {
+    "revision": "6de8c4964a8a819990be",
+    "url": "/minhvuong1.github.io/chat-cord/static/js/2.4107159a.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/minhvuong1.github.io/chat-cord/static/js/2.4107159a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb39e52543dd0cfea743",
+    "url": "/minhvuong1.github.io/chat-cord/static/js/main.b8d9d119.chunk.js"
+  },
+  {
+    "revision": "2dce01fe59527f7242b9",
+    "url": "/minhvuong1.github.io/chat-cord/static/js/runtime-main.a9f9b67f.js"
+  }
+]);
